b1f9adf1e98e660d8cfed6cb6381de3bcffe8d5f
Mr. Freeze <bivenswa@gmail.com>
2016-07-26 16:12:20 -0400

    This is a corrected EighthLab.

--- Lab/src/lab/EighthLab.java
+++ Lab/src/lab/EighthLab.java
@@ -11,20 +11,27 @@ public class EighthLab {
 		ArrayList<Float> noZeros = new ArrayList<Float>();
 
 		System.out.println("Welcome to Grand Circus Stadium");
+			
 		System.out.println("Enter times at bat: ");
 		
-		Scanner sc = new Scanner (System.in);
-		int inputAtBat = Integer.parseInt(sc.nextLine()); 
-
-		float[] timesAtBat = new float[inputAtBat];
-
+		int bats = 0;
+		
 		System.out.println("0 = Out, 1 = Single, 2 = Double, 3 = Triple, 4 = Home Run");
+		
 		float sum = 0;
-
+				
+		Scanner sc = new Scanner (System.in);
+		bats = Integer.parseInt(sc.nextLine()); 
+		float[] timesAtBat = new float[bats];
+		System.out.println(timesAtBat.length);
+		
 		for (int i = 0; i < timesAtBat.length; i++) {
+			int x=i+1;
+			System.out.println("Enter input number for "+ x);
 			int inputWhatHappened = Integer.parseInt(sc.nextLine());
 			timesAtBat[i] = inputWhatHappened;
 			sum += timesAtBat[i];
+			
 		}
 		System.out.println("Slugging Percentage:" + sum / timesAtBat.length);
 
